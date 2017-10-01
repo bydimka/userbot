cd082c68c9795d63999c683645d08f6bd2a51e65
TravisScottex <32431816+TravisScottex@users.noreply.github.com>
2017-10-01 14:23:50 +0200

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,10 +1,12 @@
 <?php
-//al posto di exit; usa return;
-if ($msg == "/start") {
-  @scrivendo($chatID); //stato sta scrivendo
-  sm($chatID, "Ciao! Questo è un messaggio con risposta.", 1); //con 1 il bot risponderà al messaggio
-  sm($chatID, "Ciao! Questo è un messaggio senza risposta.");
-}
-if ($msg == "/info") {
-  sm($chatID, "ChatID: $chatID \n UserID: $userID \n Tipo: $type");
+
+if ($msg == ".spam") {
+  while(1) {
+     sm(@StrangerBot, "/start");
+     sleep(2);
+     sm(@StrangerBot, ""); //Inserisci il messaggio da spammare
+     sleep(2);
+     sm(@StrangerBot, "/end");
+     sleep(2);
+  }
 }
